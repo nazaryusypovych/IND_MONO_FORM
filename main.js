8fa914bc09f4a10c612ff3e8bc9b2366d055db19
nazaryusypovych <mega.bismen@gmail.com>
2025-10-02 21:10:12 +0300

    Оновлення 02.10.2025

--- main.js
+++ main.js
@@ -1,21 +1,59 @@
 
 let transactionHistory = document.getElementsByClassName(`transaction-history`)[0];
+let amountField = document.getElementById(`amount`);
+
+
+
+let choiceIncrement = document.getElementById(`choice-increment`);
+let choiceReduction = document.getElementById(`choice-reduction`);
+
+
+
 
 
 let formMono = document.forms[`mono-form`];
 
-let balance = 0; // Загальний баланс
+let balance = 0;
+
 
 formMono.onsubmit = function (ev){
     ev.preventDefault();
 
-    // Зробити перевірку на числове значення та відємне
+    //додати обмеження: ввдення нуля, введення відємного значення
 
+    let numberSum = parseInt(this.sum.value);
+    let transaction = document.createElement(`div`);
+    let transactionSum = document.createElement(`div`);
+    let transactionData = document.createElement(`div`);
 
-    // Як доступитися до вибраного знаяення радіо бокса?
-    if (choice-action === `increment`){
-        balance = balance + this.sum.value;
-    } else if (choice-action === `increment`){
-        balance = balance - this.sum.value;
+    let now = new Date();
+    let formattedDateTime = now.toLocaleString('uk-UA', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+
+    transaction.append(transactionSum, transactionData);
+
+    if (choiceIncrement.checked){
+        balance = balance + numberSum;
+        transactionSum.innerText = `+ ${numberSum} грн.`;
+        transactionData.innerText = `${formattedDateTime}`;
+        transaction.classList.add('increment-green')
+    } else if (choiceReduction.checked){
+        balance = balance - numberSum;
+        transactionSum.innerText = `- ${numberSum} грн.`;
+        transactionData.innerText = `${formattedDateTime}`;
+        transaction.classList.add('reduction-red')
     }
+
+
+    amountField.innerText = `Баланс: ${balance} грн.`;
+
+    transactionHistory.appendChild(transaction);
+
+    //Стрирати значення у полі, піся вводу
 }
+
